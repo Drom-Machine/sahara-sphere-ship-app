63e73248e09c248d400aa1e81439f05084f7be93
Steve <propgmachine@gmail.com>
2022-02-17 17:40:46 -0500

    commit all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import data from "./components/back/Data/Data";
+import Header from "./components/front/Header/Header";
+import Routes from "./components/front/Routes/Routes";
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 const App = () => {
-  return <div>App</div>;
-}
 
-export default App
+  const { productItems } = data;
+  
+  return (
+  
+    <div>
+      <Router>
+        <Header />
+        <Routes productItems={productItems}/>
+      </Router>
+    </div>
+
+  );
+
+};
+
+export default App;
--- src/components/back/data/Data.js
+++ src/components/back/data/Data.js
@@ -4,80 +4,82 @@ const data = {
             id: "1",
             name: "SandDunes",
             price: 999,
-            image: "Sandsphere_SandDunes.png"
+            image: "./pics/Sandsphere_SandDunes.jpg",
         },
         {
             id: "2",
             name: "SandyBeach",
             price: 999,
-            image: "Sandsphere_SandyBeach.png"
+            image: "./pics/Sandsphere_SandyBeach.jpg",
         },
         {
             id: "3",
             name: "RedPlanet",
             price: 999,
-            image: "Sandsphere_RedPlanet.png"
+            image: "./pics/Sandsphere_RedPlanet.jpg",
         },
         {
             id: "4",
             name: "Outback",
             price: 999,
-            image: "Sandsphere_Outback.png"
+            image: "./pics/Sandsphere_Outback.jpg",
         },
         {
             id: "5",
             name: "MoonOverManhatten",
             price: 999,
-            image: "Sandsphere_MoonOverManhatten.png"
+            image: "./pics/Sandsphere_MoonOverManhatten.jpg",
         },
         {
             id: "6",
             name: "ModernCabin",
             price: 999,
-            image: "Sandsphere_ModernCabin.png"
+            image: "./pics/Sandsphere_ModernCabin.jpg",
         },
         {
             id: "7",
             name: "MistyShores",
             price: 999,
-            image: "Sandsphere_MistyShores.png"
+            image: "./pics/Sandsphere_MistyShores.jpg",
         },
         {
             id: "8",
             name: "GrassValley",
             price: 999,
-            image: "Sandsphere_GrassValley.png"
+            image: "./pics/Sandsphere_GrassValley.jpg",
         },
         {
             id: "9",
             name: "ColorfulCaves",
             price: 999,
-            image: "Sandsphere_ColorfulCaves.png"
+            image: "./pics/Sandsphere_ColorfulCaves.jpg",
         },
         {
             id: "10",
             name: "CitySlums",
             price: 999,
-            image: "Sandsphere_CitySlums.png"
+            image: "./pics/Sandsphere_CitySlums.jpg",
         },
         {
             id: "11",
             name: "CityPark",
             price: 999,
-            image: "Sandsphere_CityPark.png"
+            image: "./pics/Sandsphere_CityPark.jpg",
         },
         {
             id: "12",
             name: "CityPool",
             price: 999,
-            image: "Sandsphere_CityPool.png"
+            image: "./pics/Sandsphere_CityPool.jpg",
         },
         {
             id: "13",
             name: "AbandonedApartment",
             price: 999,
-            image: "Sandsphere_AbandonedApartment.png"
+            image: "./pics/Sandsphere_AbandonedApartment.jpg",
         },
 
-    ]
-}
+    ],
+};
+
+export default data;
