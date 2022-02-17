e0c75c68db35c89c2c259f92b5a3d06a738b9b03
Steve <propgmachine@gmail.com>
2022-02-17 13:50:42 -0500

    first commit

---
+++ src/components/back/data/Data.js
@@ -0,0 +1,83 @@
+const data = {
+    productItems: [
+        {
+            id: "1",
+            name: "SandDunes",
+            price: 999,
+            image: "Sandsphere_SandDunes.png"
+        },
+        {
+            id: "2",
+            name: "SandyBeach",
+            price: 999,
+            image: "Sandsphere_SandyBeach.png"
+        },
+        {
+            id: "3",
+            name: "RedPlanet",
+            price: 999,
+            image: "Sandsphere_RedPlanet.png"
+        },
+        {
+            id: "4",
+            name: "Outback",
+            price: 999,
+            image: "Sandsphere_Outback.png"
+        },
+        {
+            id: "5",
+            name: "MoonOverManhatten",
+            price: 999,
+            image: "Sandsphere_MoonOverManhatten.png"
+        },
+        {
+            id: "6",
+            name: "ModernCabin",
+            price: 999,
+            image: "Sandsphere_ModernCabin.png"
+        },
+        {
+            id: "7",
+            name: "MistyShores",
+            price: 999,
+            image: "Sandsphere_MistyShores.png"
+        },
+        {
+            id: "8",
+            name: "GrassValley",
+            price: 999,
+            image: "Sandsphere_GrassValley.png"
+        },
+        {
+            id: "9",
+            name: "ColorfulCaves",
+            price: 999,
+            image: "Sandsphere_ColorfulCaves.png"
+        },
+        {
+            id: "10",
+            name: "CitySlums",
+            price: 999,
+            image: "Sandsphere_CitySlums.png"
+        },
+        {
+            id: "11",
+            name: "CityPark",
+            price: 999,
+            image: "Sandsphere_CityPark.png"
+        },
+        {
+            id: "12",
+            name: "CityPool",
+            price: 999,
+            image: "Sandsphere_CityPool.png"
+        },
+        {
+            id: "13",
+            name: "AbandonedApartment",
+            price: 999,
+            image: "Sandsphere_AbandonedApartment.png"
+        },
+
+    ]
+}
